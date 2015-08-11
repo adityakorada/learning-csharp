d25ebcb228e4ce8126404cab3b6853b1464330c9
kneerunjun <kneerunjun@outlook.com>
2015-08-11 15:54:34 +0530

    native variables demo

--- consoleapplication/Program.cs
+++ consoleapplication/Program.cs
@@ -8,6 +8,48 @@ namespace consoleapplication {
     class Program {
         static void Main(string[] args) {
             //https://github.com/engiacad/learning-csharp
-        }
+            // Win 8
+            //.NET 4.5
+            //VS 2013  - Update 4 Ultimate
+            // what exactly is a program ?
+
+            // data <--> logic
+
+            //data 
+                // variables
+                   // native
+                        // stored in Place A in the memory 
+                   // object / user defined types
+                        // stored in Place B in the memory 
+                // constants
+
+            //variables :
+            // Life cycle of the varable ..
+            
+            //declare
+                //type **
+                // name / indetifier **
+                // prevalue --
+            //use
+            //dispose
+
+            int count = 10;
+            count = 190;
+
+            int anotherCount = 11;
+            anotherCount = 189;
+
+            string name = "niranjan awati";
+            name = "jsdkldfjklsdjfdsjf";
+
+            char sym = '@';
+
+            float precision = 10.89f;
+            double accuracy = 190.9023435345;
+
+            bool flag = true;
+
+
+      }
     }
 }
